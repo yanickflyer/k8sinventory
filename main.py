8ff925c68d88b0a9e43d03deb9d0bf9a9d1c249e
yanickflyer <yanick76@live.com>
2025-11-22 09:40:32 +0400

    remove unused file

--- main.py
+++ main.py
@@ -7,14 +7,6 @@ def main():
     parser.add_argument('--csv', dest='csv_filepath',help='Set filepath export report to CSV. If FilePath not specified it uses current directory', metavar='PATH')
     args = parser.parse_args()
 
-
-    # parser.add_argument('--
-    # Check if user specified --out csv
-    # if "--full" in sys.argv:
-    #     final_report = generate_inventory.get_report(minimal=False)
-    #     cluster_name = final_report['cluster_name'] if final_report else 'default_cluster_name'
-    #     print(json.dumps(final_report))
-    #     export_csv(final_report, cluster_name, minimal=False)
     if args.workload:
         print("Generating Workload report...")
         filepath = args.csv_filepath if args.csv_filepath else None
@@ -23,7 +15,6 @@ def main():
         print("Generating Full Workload report...")
         filepath = args.csv_filepath if args.csv_filepath else None
         fullworkload.get_report(filepath)
-        # export_csv(final_report, cluster_name, minimal=False)
 
 if __name__ == "__main__":
     main()
